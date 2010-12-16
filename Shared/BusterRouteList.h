b4dc82c78b5283014f5694596c1be53e77ddf399
Andrew Shepard <shep.andy@gmail.com>
2010-12-16 18:25:39 -0500

    adding in third party libraries for network connectivity and xml processing.  begin model setup.

---
+++ Shared/BusterRouteList.h
@@ -0,0 +1,19 @@
+//
+//  BusterRouteList.h
+//  Buster
+//
+//  Created by Andrew Shepard on 12/16/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SynthesizeSingleton.h"
+
+
+@interface BusterRouteList : NSObject {
+
+}
+
++ (BusterRouteList *)sharedBusterRouteList;
+
+@end
---
+++ Shared/BusterRouteList.m
@@ -0,0 +1,16 @@
+//
+//  BusterRouteList.m
+//  Buster
+//
+//  Created by Andrew Shepard on 12/16/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "BusterRouteList.h"
+
+
+@implementation BusterRouteList
+
+SYNTHESIZE_SINGLETON_FOR_CLASS(BusterRouteList);
+
+@end
