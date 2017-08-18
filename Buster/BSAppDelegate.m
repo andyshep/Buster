486375519a1d31e7777d6128f63f465193ddd670
Andrew Shepard <shep.andy@gmail.com>
2017-08-18 09:46:31 -0400

    Cleanup AppDelegate
    Remove support for iPad; will readd later.

--- Buster/BSAppDelegate.m
+++ Buster/BSAppDelegate.m
@@ -7,14 +7,11 @@
 //
 
 #import "BSAppDelegate.h"
-
 #import "BSRoutesViewController.h"
-#import "BSMapViewController.h"
 
 @interface BSAppDelegate ()
 
 @property (nonatomic, strong) BSRoutesViewController *routesViewController;
-@property (nonatomic, strong) BSMapViewController *mapViewController;
 
 @end
 
@@ -26,38 +23,10 @@
     self.routesViewController = [[BSRoutesViewController alloc] init];
     self.navigationController = [[UINavigationController alloc] initWithRootViewController:_routesViewController];
     
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        // only create a map and split view for the iPad
-        self.mapViewController = [[BSMapViewController alloc] initWithNibName:@"MapView_iPad" bundle:nil];
-        self.splitViewController = [[UISplitViewController alloc] init];
-        
-        _splitViewController.viewControllers = @[_navigationController, _mapViewController];
-        _splitViewController.delegate = _mapViewController;
-        
-        // split view is root on iPad
-        _window.rootViewController = _splitViewController;
-    } else {
-        // otherwise nav controller is root on iPhone
-        _window.rootViewController = _navigationController;
-    }
-    
+    [_window setRootViewController:_navigationController];
     [_window makeKeyAndVisible];
     
     return YES;
 }
-
-- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
-    NSLog(@"applicationDidReceiveMemoryWarning");
-}
-
-//- (void)loadPredictionsForVehicle:(NSString *)vehicle runningRoute:(NSString *)route atEpochTime:(NSString *)time {
-//    NSLog(@"loadPredictionsForVehicle: %@ runningRoute: %@ atEpochTime: %@", vehicle, route, time);
-//    
-//    mapViewController.vehicle = vehicle;
-//    mapViewController.route = route;
-//    mapViewController.time = time;
-//    
-//    [mapViewController dropPinForLocation];
-//}
 
 @end
